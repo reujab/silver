7e9b3dd7c81ad2da82123bb4c2ad1412350bc1c0
reujab <reujab@gmail.com>
2018-07-07 02:24:21 -0400

    added print subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate clap;
 
+mod print;
+
 use clap::App;
 use clap::AppSettings;
 use std::env;
@@ -10,15 +12,31 @@ fn main() {
     let matches = App::new("bronze")
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .subcommand(clap::SubCommand::with_name("init"))
+        .subcommand(
+            clap::SubCommand::with_name("print").arg(
+                clap::Arg::with_name("segments")
+                    .required(true)
+                    .min_values(1),
+            ),
+        )
         .get_matches();
 
-    match matches.subcommand().0 {
+    match matches.subcommand_name().unwrap() {
         "init" => match shell.as_str() {
             "bash" => print!("{}", include_str!("init.bash")),
             "zsh" => print!("{}", include_str!("init.zsh")),
             "fish" => print!("{}", include_str!("init.fish")),
             _ => panic!("unknown $BRONZE_SHELL"),
         },
+        "print" => print::prompt(
+            shell,
+            matches.subcommand_matches("print").unwrap().args["segments"]
+                .vals
+                // convert OsStrs to Strings, which are Sized
+                .iter()
+                .map(|s| s.to_str().unwrap().to_string())
+                .collect::<Vec<String>>(),
+        ),
         _ => panic!(),
     }
 }
---
+++ src/print.rs
@@ -0,0 +1,3 @@
+pub fn prompt(shell: String, segments: Vec<String>) {
+    // TODO
+}
