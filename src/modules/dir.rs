1161050291aec121e09cf36652862973f90960d3
reujab <reujab@gmail.com>
2018-07-08 18:04:59 -0400

    modified segment definition

--- src/modules/dir.rs
+++ src/modules/dir.rs
@@ -1,7 +1,7 @@
 use std::env;
 use Segment;
 
-pub fn dir_segment(segment: &mut Segment, _: &[&str]) {
+pub fn segment(segment: &mut Segment, _: &[&str]) {
     // TODO: support short names
     segment.value = env::current_dir().unwrap().to_str().unwrap().to_owned()
 }
--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -2,11 +2,9 @@ use Segment;
 
 mod dir;
 
-use modules::dir::dir_segment;
-
 pub fn handle(module: &str, segment: &mut Segment, args: &[&str]) {
     match module {
-        "dir" => dir_segment(segment, args),
+        "dir" => dir::segment(segment, args),
         _ => panic!("unknown module, {}", module),
     }
 }
