7b3138a38d9c225788e624e593b968073dce04b2
John Meow <j0hn.meow@ya.ru>
2020-07-06 14:05:52 +0300

    Print space from the Rust, not from the shell

--- src/init.fish
+++ src/init.fish
@@ -1,7 +1,6 @@
 function fish_prompt
     test -n "$SILVER_LEFT"; or set -l SILVER_LEFT $SILVER
     env code=$status jobs=(count (jobs -p)) cmdtime={$CMD_DURATION} silver lprint $SILVER_LEFT
-    echo -n ' '
 end
 function fish_right_prompt
     env code=$status jobs=(count (jobs -p)) cmdtime={$CMD_DURATION} silver rprint $SILVER_RIGHT
--- src/init.zsh
+++ src/init.zsh
@@ -6,7 +6,7 @@ preexec() {
 }
 
 precmd() {
-  PROMPT="$(code=$? jobs=$(jobs | wc -l) cmdtime=$(($(date +%s%3N)-$SILVER_START)) silver lprint ${SILVER_LEFT:-$SILVER}) "
+  PROMPT="$(code=$? jobs=$(jobs | wc -l) cmdtime=$(($(date +%s%3N)-$SILVER_START)) silver lprint ${SILVER_LEFT:-$SILVER})"
   ZLE_RPROMPT_INDENT=0
   RPROMPT="$(code=$? jobs=$(jobs | wc -l) cmdtime=$(($(date +%s%3N)-$SILVER_START)) silver rprint $SILVER_RIGHT)"
   SILVER_START=$(date +%s%3N)
