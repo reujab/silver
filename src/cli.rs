2d8bf5737b8927ea5b21ed9b1f302d2aa0031524
Zack Weinberg <zackw@panix.com>
2020-11-24 14:38:55 -0500

    Improve --help text.
    All of the options and subcommands now have one-line descriptions,
    and the after_help text gives a basic explanation of how to set up
    silver as well as referring to the wiki.

--- src/cli.rs
+++ src/cli.rs
@@ -4,11 +4,22 @@ pub use structopt::StructOpt;
 #[structopt(
     about = "a cross-shell customizable powerline-like prompt with icons",
     name = "silver",
-    after_help = "https://github.com/reujab/silver/wiki"
+    after_help = r#"To use silver, put `eval $(silver init)` or equivalent in your shell's
+interactive startup file.  The `lprint` and `rprint` subcommands are
+used by the code emitted by `silver init`; you should not need to use
+them directly.
+
+silver expects a configuration file in $XDG_CONFIG_HOME/silver/config.toml
+on Unix, or {FOLDERID_RoamingAppData}/silver/config/config.toml on Windows.
+(This path can be overridden with the --config option.)
+See <https://github.com/reujab/silver/wiki> for documentation of this file.
+"#
 )]
 pub struct Silver {
+    /// Path of the configuration file to use.
     #[structopt(short, long)]
     pub config: Option<String>,
+    /// Name of your shell (e.g. bash).
     #[structopt(short, long)]
     pub shell:  Option<String>,
     #[structopt(subcommand)]
@@ -17,7 +28,10 @@ pub struct Silver {
 
 #[derive(StructOpt, Debug)]
 pub enum Command {
+    /// Emit shell code to set up silver.
     Init,
+    /// Print the left side of a prompt.
     Lprint,
+    /// Print the right side of a prompt.
     Rprint,
 }
