c1eeedda768a585bc9b3291b581c62f4ca629c30
anon <anon@ymous.de>
2021-04-15 16:20:24 +0200

    beautify usercmd.rs

--- src/modules/usercmd.rs
+++ src/modules/usercmd.rs
@@ -3,12 +3,9 @@ use crate::{Segment, CONFIG};
 use std::process::Command;
 
 pub fn segment(segment: &mut Segment, _: &[&str]) {
-
-
-
-	let output = Command::new(&CONFIG.usercmd.command).args(&CONFIG.usercmd.args).output().expect("failed to execute process");	
-
-	//TODO: use String::from_utf8_lossy
+	let output = Command::new(&CONFIG.usercmd.command)
+		.args(&CONFIG.usercmd.args)
+		.output()
+		.expect("failed to execute process");	
 	segment.value = String::from_utf8(output.stdout).expect("");
-
 }
